57b680e3c036258ea72d070674f7682909208b89
web3ten0 <web3ten0@gmail.com>
2022-07-11 01:19:13 +0900

    added id of book

--- model/src/dummy_data.rs
+++ model/src/dummy_data.rs
@@ -3,12 +3,15 @@ use super::Book;
 pub fn books() -> Vec<Book> {
     vec![
         Book {
+            id: "1".into(),
             title: String::from("book1"),
         },
         Book {
+            id: "2".into(),
             title: String::from("book2"),
         },
         Book {
+            id: "3".into(),
             title: String::from("book3"),
         },
     ]
--- model/src/lib.rs
+++ model/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod dummy_data;
 
-use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, ID};
 
 pub type AppSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
 
@@ -13,16 +13,21 @@ impl QueryRoot {
     }
     pub async fn book(&self) -> Book {
         Book {
+            id: "0".into(),
             title: "book1".to_string(),
         }
     }
 }
 
 pub struct Book {
+    pub id: ID,
     pub title: String,
 }
 #[Object]
 impl Book {
+    pub async fn id(&self) -> ID {
+        self.id.clone()
+    }
     pub async fn title(&self) -> String {
         self.title.clone()
     }
